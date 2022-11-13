c3db952ce6246add3b8d5495666fc5c98c6f9a16
Paris King <106052038+finalmock@users.noreply.github.com>
2022-11-13 19:51:03 +0530

    Add files via upload

---
+++ DLL.java
@@ -0,0 +1,14 @@
+//
+//public class DLL {
+//	jtextfield1.gettext();
+//	jtextfield2.gettext();
+//	
+//	int a,b;
+//	a=Interger.parseInt(jtextfield1.gettext());
+//	b=Interger.parseInt(jtextfield2.gettext());
+//	
+//	int c=a+b;
+//	
+//	jtextfield.settext(c);
+//	
+//}
